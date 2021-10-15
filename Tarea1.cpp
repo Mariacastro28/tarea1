dfbee1cedf890209658114c317ded5861bab2774
Maria Castro <92470095+Mariacastro28@users.noreply.github.com>
2021-10-14 21:31:47 -0400

    Add files via upload

---
+++ Tarea1.cpp
@@ -0,0 +1,18 @@
+include <iostream>
+using namespace std;
+
+int main(int argc,char *argv[]) {
+float a,b,suma,resta,multi,divi;
+cout<< 1er #: ";cin>>a;
+cout<< 2do #: ";cin>>b;
+
+suma=a+b;
+resta=a-b;
+multi=a*b;
+divi=a/b;
+cout<<"suma es: "<<a<<"+"<<b<<"="<<suma<<endl;
+cout<<"resta es: "<<a<<"-"<<b<<"="<<resta<<endl;
+cout<<"multi es: "<<a<<"*"<<b<<"="<<multi<<endl;
+cout<<"divi es: "<<a<<"/"<<b<<"="<<divi<<endl;
+return 0;
+}
